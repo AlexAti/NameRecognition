f2285dcf364280942c67d223a248ea8aa0b914a2
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2020-06-29 11:59:52 +0200

    increase test

--- tests/test.py
+++ tests/test.py
@@ -8,11 +8,11 @@ time.sleep(120)
 print('Tiempo finalizado')
 session = requests.Session()
 
-m = 50000
+m = 100000
 n = 5
 cluster = []
 
-for i in range(5):
+for i in range(m):
     url = 'http://namerecognition_name_recognition_{}:5000/screening/'.format(random.randint(1,n))
     respond = session.get(
         url = url,
