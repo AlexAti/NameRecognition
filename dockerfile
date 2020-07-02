9fc11ce3f4ab7a221679e9405182dbeccb8b3d80
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2020-07-03 01:06:30 +0200

    update dockerfile

--- dockerfile
+++ dockerfile
@@ -5,8 +5,9 @@ RUN git clone https://rojo1997:ogame522@github.com/rojo1997/NameRecognition
 RUN python3 -m pip install -r /NameRecognition/requirements.txt
 WORKDIR /NameRecognition/
 EXPOSE 5000
-CMD [ "python3", "NameRecognition/Synthetic.py"]
+CMD [ "python3", "NameRecognition/app.py"]
 
 FROM postgres:latest as nr_db
 ADD postgresql.conf /usr/share/postgresql/
 COPY sql/ /docker-entrypoint-initdb.d/
+EXPOSE 5432
--- sql/insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO WLF.screening (key_screen,value_screen,birth_country) VALUES ('screen_1', 'Ernesto Martínez del Pino', 'ES');
-INSERT INTO WLF.screening (key_screen,value_screen,birth_country) VALUES ('screen_2', 'Ernesto Martínez Pino', 'FR');
-INSERT INTO WLF.screening (key_screen,value_screen,birth_country) VALUES ('screen_3', 'Ernesto del Pino', 'ES');
-INSERT INTO WLF.screening (key_screen,value_screen,birth_country) VALUES ('screen_4', 'Ernesto Martínez del Pino', 'ES');
-INSERT INTO WLF.screening (key_screen,value_screen,birth_country) VALUES ('screen_5', 'Ernesto Martínez del', 'USA');
