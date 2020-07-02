1d8c35dfee354608f6b237391ed2610d04ad82bc
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2020-07-02 14:29:33 +0200

    Add create.sql

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
 		NameRecognition es un módulo escrito en Python pensado para detectar patrones de similitud complejos entre cádenas de texto de longitud corta.
 	""",
 	author = 'Ernesto Martínez del Pino',
-	packages = ['NameRecognition']
+	packages = ['NameRecognition'],
+	install_requires = [i.strip() for i in open('requirements.txt').readlines()],
+	test_suite = 'tests'
 )
--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,29 @@
-CREATE TABLE client (
-    
-)
+CREATE TABLE party (
+    key_party VARCHAR(64) PRIMARY KEY,
+    value_party VARCHAR(128),
+    birth_date DATE,
+    birth_country VARCHAR(16),
+    identifier VARCHAR(64)
+);
+
+CREATE TABLE screening (
+    key_screen VARCHAR(64) PRIMARY KEY,
+    value_screen VARCHAR(128),
+    birth_date DATE,
+    birth_country VARCHAR(16),
+    identifier VARCHAR(64)
+);
+
+CREATE TABLE score_factor (
+    factor_key VARCHAR(64) PRIMARY KEY,
+    value_factor FLOAT,
+    birth_date_factor FLOAT,
+    birth_country_factor FLOAT,
+    identifier_factor FLOAT
+);
+
+CREATE TABLE threshold (
+    threshold_key VARCHAR(64),
+    value_threshold FLOAT,
+    global_threshold FLOAT
+);
