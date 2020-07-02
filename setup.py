b0be0e9935520b064cb0ce7ad70aeb8756cf82be
Ernesto Martinez del Pino <ernestomar1997@hotmail.com>
2020-07-02 14:13:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = 'NameRecognition',
+	version = '0.1',
+	description = """
+		NameRecognition es un módulo escrito en Python pensado para detectar patrones de similitud complejos entre cádenas de texto de longitud corta.
+	""",
+	author = 'Ernesto Martínez del Pino',
+	packages = ['NameRecognition']
+)
